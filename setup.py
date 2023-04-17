aa0989f0c220fa655f8a345fe200bf25d6b6fdbe
RekhuGopal <vrchinnarathod@gmail.com>
2023-04-17 09:23:51 +0530

    Example Python packages

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudquicklabs1",
+    version="0.0.1",
+    author="Rekhu Chinnarathod",
+    author_email="vrchinnarathod@gmail.com",
+    url="https://www.youtube.com/channel/UCv9MUffHWyo2GgLIDLVu0KQ",
+    description="An application that informs you of the time in different locations and timezones",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["click", "pytz"],
+    entry_points={"console_scripts": ["timechecker = src.main:main"]},
+)
