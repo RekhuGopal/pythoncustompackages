63ec22e2bad2badedc38111039b3bffc4fbf88f9
RekhuGopal <vrchinnarathod@gmail.com>
2023-04-20 09:27:04 +0530

    Save

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloudquicklabs1",
-    version="0.0.3",
+    version="0.0.4",
     author="Rekhu Chinnarathod",
     author_email="vrchinnarathod@gmail.com",
     url="https://www.youtube.com/channel/UCv9MUffHWyo2GgLIDLVu0KQ",
