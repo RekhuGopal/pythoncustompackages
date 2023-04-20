d466a9e7e5a15445819efbd98b99b15e7374570f
RekhuGopal <vrchinnarathod@gmail.com>
2023-04-20 08:51:08 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -1,53 +1,20 @@
-from datetime import datetime
-import pytz
+import click
+from src.logic import area, area_zone
 
-def area(location):
-    """This function takes in a location as argument, checks the list of locations available and returns the formatted time to the user."""
-    location = format_location(location)
-    for areas in pytz.all_timezones:
-        if location.lower() in areas.lower():
-            location = areas
-            tz = pytz.timezone(location)
-            date_now = datetime.now(tz)
-            formatted_date = date_now.strftime("%B %d, %Y %H:%M:%S")
-            print(f"{location} time: ", formatted_date)
-            break
+@click.command()
+@click.option(
+    "--location",
+    help="This specifies the location you want to know the time. For example, Lagos or London",
+)
+@click.option(
+    "--zone",
+    help="The timezone information you need. Ensure it is properly capitalized, for example CET or WAT",
+)
+def main(location, zone):
+    if location:
+        area(location)
+    if zone:
+        area_zone(zone)
 
-    else:
-        print("This location isn't on the tz database on Wikipedia")
-
-def area_zone(zone):
-    """This function takes in a time zone as argument, checks the list of timezones and returns the formatted time to the user."""
-    try:
-        zone = timezones(zone)
-        tz = pytz.timezone(zone)
-        date_now = datetime.now(tz)
-        formatted_date = date_now.strftime("%B %d, %Y %H:%M:%S")
-        print(f"{zone} time: ", formatted_date)
-
-    except Exception:
-        print("Timezone is not on the list. Consider using location instead.")
-
-def timezones(zone):
-    """This function is used to handle situations of Daylight Saving Time that the standard library can't recognize."""
-    zones = {
-        "PDT": "PST8PDT",
-        "MDT": "MST7MDT",
-        "EDT": "EST5EDT",
-        "CDT": "CST6CDT",
-        "WAT": "Etc/GMT+1",
-        "ACT": "Australia/ACT",
-        "AST": "Atlantic/Bermuda",
-        "CAT": "Africa/Johannesburg",
-    }
-
-    try:
-        zones[zone]
-
-    except:
-        return zone
-    return zones[zone]
-
-def format_location(location):
-    location = location.replace(" ", "_")
-    return location
+if __name__ == "__main__":
+    main()
